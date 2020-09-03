73fa8eaa5bc2cbb458b6b221bf6b803297e9a0ec
marvin evins <marvinevins@yahoo.com>
2020-09-02 20:15:55 -0500

    working on challenge set 1

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -5,32 +5,62 @@ namespace ChallengeSets
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if ( num1 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            double sum = 0;
+            sum = minuend - subtrahend
+            return sum;
+
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            double sum = 0;
+            sum = minuend + subtrahend
+            return sum;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (number1 < number)
+            {
+                return number1;
+            } else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            long multSum = 0;
+            multSum = factor1 * factor2;
+            return multSum;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            //if nameOfPerson != "" ?
+            return (nameOfPerson == "") ? "Hello" : $"hello, {nameOfPerson }";
+
         }
 
         public string GetHey()
